f5e92fd33926ae9870ada028476003bad462ae40
kyoukaya <kyoukaya@gmail.com>
2020-03-01 00:44:54 +0800

    update flags

--- cmd/main.go
+++ cmd/main.go
@@ -9,13 +9,15 @@ import (
 	"github.com/kyoukaya/rhine/proxy"
 )
 
+var logPath = flag.String("log-path", "logs/proxy.log", "file to output the log to")
+var silent = flag.Bool("silent", false, "don't print anything to stdout")
+var filter = flag.Bool("filter", false, "enable the host filter")
+var verbose = flag.Bool("v", false, "print Rhine verbose messages")
+var verboseGoProxy = flag.Bool("v-goproxy", false, "print verbose goproxy messages")
+var host = flag.String("host", ":8080", "hostname:port")
+var disableCertStore = flag.Bool("disable-cert-store", false, "disables the built in certstore, reduces memory usage but increases HTTP latency and CPU usage")
+
 func main() {
-	logPath := flag.String("log-path", "logs/proxy.log", "file to output the log to")
-	silent := flag.Bool("silent", false, "don't print anything to stdout")
-	filter := flag.Bool("filter", false, "enable the host filter")
-	verbose := flag.Bool("v", false, "print Rhine verbose messages")
-	verboseGoProxy := flag.Bool("v-goproxy", false, "print verbose goproxy messages")
-	host := flag.String("host", ":8080", "hostname:port")
 	flag.Parse()
 
 	options := &proxy.Options{
@@ -26,6 +28,7 @@ func main() {
 		Verbose:          *verbose,
 		VerboseGoProxy:   *verboseGoProxy,
 		Address:          *host,
+		DisableCertStore: *disableCertStore,
 	}
 	rhine := proxy.NewProxy(options)
 	rhine.Start()
